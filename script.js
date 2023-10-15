e9d8abbf66e3f4cc0b430a3b9ee3171f7ef7aa3e
Esad kasım <111705020+esadkasim@users.noreply.github.com>
2023-10-16 00:33:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".auto-type",{
+    strings:[" build life changing courses"," can help you in your network"],
+    typeSpeed:150,
+    backSpeed:50,
+    loop:true
+})
+console.log('esad')
